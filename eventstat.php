b2c60d06703a507471126b7160368bd20aecc492
Dmitry Shumilin <infernozzz@yandex.ru>
2021-05-10 15:15:52 +0300

    0.0.1: initial

---
+++ eventstat.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Eventstat
+ * Plugin URI: https://github.com/chirontex/eventstat
+ * Description: Кастомная статистика мероприятий, созданных в плагине MyEventON.
+ * Version: 0.0.1
+ * Author: Dmitry Shumilin
+ * Author URI: mailto://chirontex@yandex.ru
+ * 
+ * @package Eventstat
+ * @author Dmitry Shumilin (chirontex@yandex.ru)
+ * @since 0.0.1
+*/
